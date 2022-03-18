5bbc12c4348779c53bd7dfef3af47c9973f26a21
Dominik Horváth <32178114+Dominik1799@users.noreply.github.com>
2022-03-18 17:51:59 +0100

    Update init.js

--- mongo/init.js
+++ mongo/init.js
@@ -1,4 +1,3 @@
 db = new Mongo().getDB("ams");
 db.createCollection('articles', {storageEngine: {wiredTiger: {configString: 'block_compressor=zlib'}}})
-db.createCollection('crimemaps', {storageEngine: {wiredTiger: {configString: 'block_compressor=zlib'}}})
 db.createCollection('errorlinks', {storageEngine: {wiredTiger: {configString: 'block_compressor=zlib'}}})
