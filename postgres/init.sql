26c3d3b8ad5e8941ab693b86ba6149a6648f897b
David Silady <40362861+DavidSilady@users.noreply.github.com>
2022-03-26 15:00:56 +0100

    Update init.sql

--- postgres/init.sql
+++ postgres/init.sql
@@ -1,5 +1,5 @@
 -- we don't know how to generate root <with-no-name> (class Root) :(
-create table "user"
+create table if not exists "user"
 (
     id       serial
         constraint user_pk
@@ -11,9 +11,27 @@ create table "user"
 alter table "user"
     owner to fiitkar_postgres;
 
-create unique index user_id_uindex
+create unique index if not exists user_id_uindex
     on "user" (id);
 
-create unique index user_username_uindex
+create unique index if not exists user_username_uindex
     on "user" (username);
 
+create table if not exists refresh_token
+(
+    id              serial
+        constraint refresh_token_pk
+            primary key,
+    token           varchar                 not null,
+    max_age_seconds integer   default 300   not null,
+    last_access     timestamp default now() not null
+);
+
+alter table refresh_token
+    owner to fiitkar_postgres;
+
+create unique index if not exists refresh_token_id_uindex
+    on refresh_token (id);
+
+create unique index if not exists refresh_token_token_uindex
+    on refresh_token (token);
