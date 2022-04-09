996717ededd8205075c462e12b57b185aa3f87fd
Jakub Hlavačka <43497822+JKBGIT1@users.noreply.github.com>
2022-04-09 13:11:19 +0200

    Update init.sql

--- postgres/init.sql
+++ postgres/init.sql
@@ -1,5 +1,5 @@
 -- we don't know how to generate root <with-no-name> (class Root) :(
-create table if not exists "user"
+create table "user"
 (
     id       serial
         constraint user_pk
@@ -11,13 +11,13 @@ create table if not exists "user"
 alter table "user"
     owner to fiitkar_postgres;
 
-create unique index if not exists user_id_uindex
+create unique index user_id_uindex
     on "user" (id);
 
-create unique index if not exists user_username_uindex
+create unique index user_username_uindex
     on "user" (username);
 
-create table if not exists refresh_token
+create table refresh_token
 (
     id              serial
         constraint refresh_token_pk
@@ -30,8 +30,30 @@ create table if not exists refresh_token
 alter table refresh_token
     owner to fiitkar_postgres;
 
-create unique index if not exists refresh_token_id_uindex
+create unique index refresh_token_id_uindex
     on refresh_token (id);
 
-create unique index if not exists refresh_token_token_uindex
+create unique index refresh_token_token_uindex
     on refresh_token (token);
+
+create table pdf_report
+(
+    id          serial
+        constraint pdf_report_pk
+            primary key,
+    status      varchar default 'In Progress'::character varying not null,
+    content     json    default '[]'::json                       not null,
+    creation    date    default CURRENT_DATE                     not null,
+    user_id     integer                                          not null
+        constraint pdf_report_user_id_fk
+            references "user",
+    last_export date
+);
+
+alter table pdf_report
+    owner to fiitkar_postgres;
+
+create unique index pdf_report_id_uindex
+    on pdf_report (id);
+
+
